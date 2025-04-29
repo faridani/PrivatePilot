63855dbe70e137db70d434121c33eac5df2fb8ae
fereidani <info@fereidani.com>
2025-04-29 10:33:39 +0330

    Improve build script and linux build support

--- private-pilot-extension/build.bat
+++ private-pilot-extension/build.bat
@@ -1,5 +1,10 @@
+where npm >nul 2>nul || (
+    echo npm is not installed. Aborting.
+    exit /b 1
+)
+
 @echo off
 call npm install
 call npm run compile
-call vsce package
+call npx vsce package
 echo All commands executed successfully!
