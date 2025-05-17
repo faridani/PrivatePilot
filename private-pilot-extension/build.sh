80f7929fcb44aaefeeb16963fc7502960b18ad2a
fereidani <info@fereidani.com>
2025-05-17 11:13:15 +0330

    Add auto install functionality for unix build system

--- private-pilot-extension/build.sh
+++ private-pilot-extension/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -n "$1" ] && [ "$1" != "install" ]; then
+    echo "Invalid argument: $1"
+    echo "Usage: $0 [install]"
+    exit 1
+fi
+
+rm -rf *.vsix
 command -v npm >/dev/null 2>&1 || { echo "npm is not installed. Aborting."; exit 1; }
 
 npm install
@@ -7,3 +14,14 @@ npm run compile
 npx vsce package
 
 echo "All commands executed successfully!"
+
+
+if [ "$1" == "install" ]; then
+    VSIX_FILE=$(ls *.vsix | head -n 1)
+    if [ -f "$VSIX_FILE" ]; then
+        code --install-extension "$VSIX_FILE"
+    else
+        echo "No VSIX file found to install."
+        exit 1
+    fi
+fi
